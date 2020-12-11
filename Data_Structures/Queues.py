dc7ff2c66a0dcc58a5ac920a32e4a6e99bea4dd1
MateoMinghi <59071760+MateoMinghi@users.noreply.github.com>
2020-12-11 11:13:16 -0600

    Create Queues.py

---
+++ Data_Structures/Queues.py
@@ -0,0 +1,27 @@
+#further explanation on queues in README file  
+#python doesn't have built-in queues, but the can be implemented with lists or linked lists.
+ 
+#implementation using lists
+
+list_of_queue = [] #declare variable 'queue' which is a list that is then going to be filled in
+ 
+
+list_of_queue.append('a')
+list_of_queue.append('b')
+list_of_queue.append('c')
+ 
+print("initial list")
+print(list_of_queue) #when we print it, we see elements inside a list
+ 
+# Removing elements from the queue
+print()
+print(list_of_queue.pop(0))
+print(list_of_queue.pop(0))
+print(list_of_queue.pop(0)) #after dequeuing elements from the list, they get arranged in FIFO order
+
+
+
+print() 
+print("Queue after removing elements")
+print(list_of_queue)
+#if we print the list_of_queue after the elemnts have been dequeued, it must be empty
