915c84d580eaa3c52664d148baf9a2eb833479f4
MateoMinghi <59071760+MateoMinghi@users.noreply.github.com>
2021-06-24 18:55:16 -0500

    Update Merge_Sort.py

--- Algorithms/Merge_Sort.py
+++ Algorithms/Merge_Sort.py
@@ -1,42 +1,40 @@
 #Merge sort is a divide and conquer algorithm
 #O(n log n)
-def mergeSort(arr): 
-    if len(arr) >1: 
-        mid = len(arr)//2 
-        L = arr[:mid]   
-        R = arr[mid:]  
-  
-        mergeSort(L) # Sorting the first half 
-        mergeSort(R) # Sorting the second half 
-  
-        i = 0
-        j = 0
-        k = 0
-           
-        while i < len(L) and j < len(R): 
-            if L[i] < R[j]: 
-                arr[k] = L[i] 
-                i+= 1
-            else: 
-                arr[k] = R[j] 
-                j+= 1
-            k+= 1
+def merge_sort(arr):
+  if len(arr) > 1:
+    half = len(arr) // 2
+
+    L = arr[:half]
+    R = arr[half:]
+
+    merge_sort(L)
+    merge_sort(R)
+
+    i = 0
+    j = 0
+    k = 0
+
+    while i < len(L) and j < len(R): 
+      if L[i] < R[j]: 
+        arr[k] = L[i] 
+        i+= 1
+      else: 
+        arr[k] = R[j] 
+        j+= 1
+      k+= 1
           
         # Checking if any element was left 
-        while i < len(L): 
-            arr[k] = L[i] 
-            i+= 1
-            k+= 1
+    while i < len(L): 
+      arr[k] = L[i] 
+      i+= 1
+      k+= 1
           
-        while j < len(R): 
-            arr[k] = R[j] 
-            j+= 1
-            k+= 1
+    while j < len(R): 
+      arr[k] = R[j] 
+      j+= 1
+      k+= 1
+  return arr
 
-if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
-    print ("Given array is:")  
-    print(arr)
-    mergeSort(arr) 
-    print("Sorted array is:") 
-    print(arr)
+if __name__ == "__main__":
+  arr = [9,8,4,1,2,5,6,2,4,7,5]
+  print(merge_sort(arr))
