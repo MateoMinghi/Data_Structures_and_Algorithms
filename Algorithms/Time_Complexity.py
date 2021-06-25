0c8d273cb9e4413a1e7e055e4c67cdcf6976c766
MateoMinghi <59071760+MateoMinghi@users.noreply.github.com>
2021-06-24 19:29:06 -0500

    Update Time_Complexity.py

--- Algorithms/Time_Complexity.py
+++ Algorithms/Time_Complexity.py
@@ -1,38 +1,38 @@
 import time
 import sys
 
-#these functions return the factorial of a number 
-#factorial is a function that multiplies a number by every number below it
+#these functions return the fibonacci numbers of a number 
 
-def factorial_1(n):
-  if n == 0:
-    return 1
-  else:
-    return n * factorial_1(n - 1)
+def fib(n):    # write Fibonacci series up to n
+    a = 0 
+    b = 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
 
-def factorial(n):
-    response = 1
-    while n > 1:
-        response = response * n
-        n = n - 1
-    return response
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return 1
+    
+    else:
+        resultado = fibonacci(n - 1) + fibonacci(n - 2,)
+        return resultado
 
 
-#over here we assign n with a large value (2500)
+#over here we assign n with a large value (25)
 #that takes a lot of operations, therefore we must choose the correct method to implement it
 
 if __name__ == '__main__':
-    n = 2500
+    n = 25
     sys.setrecursionlimit(n + 10)
 
     startingTime = time.time()
-    factorial_1(n)
-    endTime = time.time()
-    print(f"Execution time with recursion\t{endTime - startingTime}");
-
-    startingTime = time.time()
-    factorial(n)
+    fib(n)
     endTime = time.time()
     print(f"Execution time with loop\t{endTime - startingTime}");
 
-    
+    startingTime = time.time()
+    fibonacci(n)
+    endTime = time.time()
+    print(f"Execution time with recursion\t{endTime - startingTime}");
